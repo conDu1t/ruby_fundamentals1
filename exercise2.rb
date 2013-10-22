04414ec55043fc4da3f4a22db460286221119547
geomese <geomese@gmail.com>
2013-10-22 13:29:57 -0400

    second exercise

---
+++ exercise2.rb
@@ -0,0 +1,19 @@
+# tip calculator
+puts 55 * 0.13
+
+# adding a string to an integer
+def add(x, y)
+	x + y
+end
+
+puts "string " + 5.to_s
+
+# multiplying numbers
+x = 45628 * 7839
+
+puts "45628 times 7839 is equal to #{x}"
+
+# true or false
+# will put true
+
+puts (true && false) || (false && true) || !(false && false)
